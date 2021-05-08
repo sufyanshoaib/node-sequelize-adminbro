63bea90ad4dcc30db9ad89077ef6212f0e006056
Muhammad Sufyan <sufyanshoaib@gmail.com>
2021-05-08 14:01:04 +0400

    Fixed public url issue

--- app/resources/admin.user.js
+++ app/resources/admin.user.js
@@ -2,7 +2,7 @@ const models = require('../models');
 const AWS = require('aws-sdk');
 const uploadFeature = require('@admin-bro/upload');
 
-const Bucket = 'publicusersdb/images';
+const Bucket = 'publicusersdb';
 
 var credentials = process.env.NODE_ENV === 'development' ? 
     new AWS.SharedIniFileCredentials({ profile: process.env.AWS_PROFILE }) : {
@@ -17,7 +17,7 @@ const userUploadOptions = {
       },
     },
     features: [uploadFeature({
-      provider: { aws: { region: process.env.AWS_REGION, bucket: Bucket, 
+      provider: { aws: { region: process.env.AWS_REGION, bucket: Bucket, expires: 0, 
                           accessKeyId: credentials.accessKeyId, 
                           secretAccessKey: credentials.secretAccessKey} },
       properties: {
@@ -25,6 +25,7 @@ const userUploadOptions = {
         mimeType: 'mimeType'
       },
       multiple: false,
+      uploadPath: (record, filename) => `images/${record.id()}/${filename}`,
       validation: {
         mimeTypes: ['image/png', 'image/jpeg']
       }
